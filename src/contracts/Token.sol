dda7c2888b6d1c2406bae66c9abab919c36a2692
Sharon Sheah <sharonsheah@gmail.com>
2021-08-15 21:23:04 +1000

    add public methods to token and tests

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,5 +2,13 @@
 pragma solidity >=0.5.0;
 
 contract Token {
+  string public name = "Yuzu Token";
+  string public symbol = "YUZU";
+  uint256 public decimals = 18;
+  uint256 public totalSupply;
+
   string public name = "My Name";
+
+  constructor() public {
+    totalSupply = 1000000 * (10 ** decimals);
 }
