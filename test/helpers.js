e46eabeb5d8ec6eacd5dc541e26043bc88222532
Sharon Sheah <sharonsheah@gmail.com>
2021-08-19 23:56:19 +1000

    create Exchange smart contract with fee account, deposit ether and token functions

--- test/helpers.js
+++ test/helpers.js
@@ -1,5 +1,9 @@
+export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000'
+
 export const EVM_REVERT = 'VM Exception while processing transaction: revert';
 
-export const tokens = (n) => {
+export const ether = (n) => {
 	return web3.utils.toWei(n.toString(), "ether");
 };
+
+export const tokens = (n) => ether(n);
